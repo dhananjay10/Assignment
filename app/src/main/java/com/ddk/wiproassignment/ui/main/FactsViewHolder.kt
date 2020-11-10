272098b2ffc80dfc2c17cfaf1e0177e975b62ff4
Dhananjay <dhananjaykulkarni19@gmail.com>
2020-11-10 11:43:55 +0530

    findViewById removed by adding Kotlin Synthetic import

--- app/src/main/java/com/ddk/wiproassignment/ui/main/FactsViewHolder.kt
+++ app/src/main/java/com/ddk/wiproassignment/ui/main/FactsViewHolder.kt
@@ -1,26 +1,24 @@
 package com.ddk.wiproassignment.ui.main
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.ddk.wiproassignment.R
 import com.ddk.wiproassignment.data.ResponseItem
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.list_item.*
 
-class FactsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    private var tvTitle: TextView = itemView.findViewById(R.id.tv_title);
-    private var tvDescription: TextView = itemView.findViewById(R.id.tv_description);
-    private var ivImage: ImageView = itemView.findViewById(R.id.iv_image);
+class FactsViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+    LayoutContainer {
 
     fun bindData(item: ResponseItem) {
-        tvTitle.text = item.title ?: tvDescription.context.getString(R.string.title_not_available)
-        tvDescription.text = item.description  ?: tvDescription.context.getString(R.string.description_not_available)
-        Glide.with(ivImage.context)
+        tv_title.text = item.title ?: tv_description.context.getString(R.string.title_not_available)
+        tv_description.text =
+            item.description ?: tv_description.context.getString(R.string.description_not_available)
+        Glide.with(iv_image.context)
             .load(item.imageHref)
             .error(R.drawable.ic_error)
-            .into(ivImage)
+            .into(iv_image)
     }
 
 }
